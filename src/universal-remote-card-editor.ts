dd77cb310ac9c32f672778078df007b80fa08d1a
Nerwyn Singh <nerwyn1994@gmail.com>
2025-08-12 22:00:46 -0400

    fix editor ha-button leading icon size

--- src/universal-remote-card-editor.ts
+++ src/universal-remote-card-editor.ts
@@ -3315,7 +3315,7 @@ export class UniversalRemoteCardEditor extends LitElement {
 			}
 			ha-button {
 				width: fit-content;
-				--mdc-icon-size: 100%;
+				--mdc-icon-size: 24px;
 			}
 			ha-button::part(label) {
 				text-transform: capitalize;
